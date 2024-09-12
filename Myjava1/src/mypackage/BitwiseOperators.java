569f89637b7ffe46845ed4c610daecfcb0a79204
Sree Vishnu <Sree Vishnu@ROWDY-VISHNU44>
2024-09-12 12:44:45 +0530

    commit8

---
+++ Myjava1/src/mypackage/BitwiseOperators.java
@@ -0,0 +1,18 @@
+package mypackage;
+
+public class BitwiseOperators {
+
+	public static void main(String[] args) {
+		int a=5; //0101
+		int b=3; //0011
+		
+		//bitwise AND
+		
+		System.out.println("a & b : " + (a&b));
+		
+		//bitwise OR
+		
+		System.out.println("a ||b : " +(a|b));
+	}
+
+}
