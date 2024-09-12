2fcd06a6d19e08ae0b1dd1948e0f97feb37e5743
Sree Vishnu <Sree Vishnu@ROWDY-VISHNU44>
2024-09-12 12:15:35 +0530

    sixth commit

---
+++ Myjava1/src/mypackage/AssignmentOperator.java
@@ -0,0 +1,20 @@
+package mypackage;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int a=10 , b=5;
+
+		System.out.println("a+=b : " +(a+=b));
+
+		System.out.println("a-=b : " +(a-=b));
+
+		System.out.println("a*=b : " +(a*=b));
+
+		System.out.println("a/=b : " +(a/=b));
+		
+		System.out.println("a%=b : " +(a%=b));
+
+	}
+
+}
