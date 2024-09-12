747416f74f4245824b7cae21afe15b51763023a3
Sree Vishnu <Sree Vishnu@ROWDY-VISHNU44>
2024-09-12 11:35:38 +0530

    thirdcommit

---
+++ Myjava1/src/mypackage/arthmeticOperatios.java
@@ -0,0 +1,21 @@
+package mypackage;
+
+public class arthmeticOperatios {
+
+	public static void main(String[] args) {
+		int a=10, b=20;
+		
+		int sum=a+b;
+		System.out.println("addition of a and  is "+sum);
+
+		int sub=a-b;
+		System.out.println("subtraction of a and  is "+sub);
+
+		int mul=a*b;
+		System.out.println("multiplication of a and  is "+mul);
+
+		int div=a/b;
+		System.out.println("Divsion of a and  is "+div);
+	}
+
+}
