a89c0e7fb36bebb2623985e1876402e6ca30904e
Sree Vishnu <Sree Vishnu@ROWDY-VISHNU44>
2024-09-12 12:29:02 +0530

    seventhcommit

---
+++ Myjava1/src/mypackage/IncrementOperators.java
@@ -0,0 +1,26 @@
+package mypackage;
+
+public class IncrementOperators {
+
+	public static void main(String[] args) {
+		int a=10;
+		//post increment
+		System.out.println("a++ = " + a++);
+
+		System.out.println("after increment" + a);
+		
+		//preincrement
+		System.out.println("++a = " + ++a);
+			
+		//postdecrement
+			System.out.println("a-- = " + a--);
+
+			System.out.println("after postdecrement " + a);
+			
+		//predecremnt
+			System.out.println("--a = " + --a);
+				
+
+	}
+
+}
