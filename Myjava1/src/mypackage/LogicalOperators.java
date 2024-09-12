b924869167b4bded0b19ac312cffda70e59b7891
Sree Vishnu <Sree Vishnu@ROWDY-VISHNU44>
2024-09-12 12:16:55 +0530

    fifth commit

---
+++ Myjava1/src/mypackage/LogicalOperators.java
@@ -0,0 +1,18 @@
+package mypackage;
+
+public class LogicalOperators {
+	public static void main(String[] args) {
+	int a=5, b=10, c=20;
+	
+	//logical AND
+	System.out.println("(a>b) && (b<c) : "+((a>b) && (b<c)));
+
+
+	//logical OR
+	System.out.println("(a>b) || (b<c) : "+((a>b) || (b<c)));
+
+	//logical NOT
+	//System.out.println("(a>b) !! (b<c) : "+((a>b)  (b<c)));
+			
+}
+}
