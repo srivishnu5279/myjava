d4b9336598e1a8ad0669eb1ec85043f0ffd37383
Sree Vishnu <Sree Vishnu@ROWDY-VISHNU44>
2024-09-12 11:55:35 +0530

    fourth commit

---
+++ Myjava1/src/mypackage/RelationalOperators.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		
+		int a=5 , b=10;
+		
+		System.out.println("is a is equals to b:" + (a==b));
+		System.out.println("is a not equals to b:" +(a!=b));
+		System.out.println("is a greater than b :" + (a>b));
+		System.out.println("is a less than b:"+(a<b));
+		System.out.println("is a greater than or equals to b:"+(a>=b));
+		System.out.println("is a less than or equals to b:"+(a<=b));
+	}
+
+}
